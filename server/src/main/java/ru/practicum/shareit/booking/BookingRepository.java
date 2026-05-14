16c007337daa2de0359ea7b81af8687f9edefee7
Макс <maksim5494@yandex.ru>
2026-05-15 02:45:27 +0500

    15

--- server/src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -11,15 +11,23 @@ import java.util.Optional;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findAllByBookerIdOrderByStartDesc(Long bookerId);
+
     List<Booking> findAllByBookerIdAndStartBeforeAndEndAfterOrderByStartDesc(Long bookerId, LocalDateTime start, LocalDateTime end);
+
     List<Booking> findAllByBookerIdAndEndBeforeOrderByStartDesc(Long bookerId, LocalDateTime now);
+
     List<Booking> findAllByBookerIdAndStartAfterOrderByStartDesc(Long bookerId, LocalDateTime now);
+
     List<Booking> findAllByBookerIdAndStatusOrderByStartDesc(Long bookerId, BookingStatus status);
 
     List<Booking> findAllByItemOwnerIdOrderByStartDesc(Long ownerId);
+
     List<Booking> findAllByItemOwnerIdAndStartBeforeAndEndAfterOrderByStartDesc(Long ownerId, LocalDateTime start, LocalDateTime end);
+
     List<Booking> findAllByItemOwnerIdAndEndBeforeOrderByStartDesc(Long ownerId, LocalDateTime now);
+
     List<Booking> findAllByItemOwnerIdAndStartAfterOrderByStartDesc(Long ownerId, LocalDateTime now);
+
     List<Booking> findAllByItemOwnerIdAndStatusOrderByStartDesc(Long ownerId, BookingStatus status);
 
     Optional<Booking> findFirstByItemIdAndItemOwnerIdAndEndBeforeAndStatusOrderByStartDesc(Long itemId, Long userId, LocalDateTime localDateTime, BookingStatus status);
--- server/src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -17,8 +17,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.stream;
-
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
